9dca43477398abb64a067efdc267a53964c57aeb
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-05 11:34:12 -0300

    feat: extract css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: 'development',
@@ -7,11 +8,17 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/,
             use: [
-                'style-loader',
+                MiniCssExtractPlugin.loader,
+                // 'style-loader',
                 'css-loader'
             ]
         }]
